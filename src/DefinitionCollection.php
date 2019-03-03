200367a4661947dd54db92f082c9534041f10c44
Programster <sdpagent@gmail.com>
2019-03-03 08:56:51 +0000

    adds collections for paths and definitions

---
+++ src/DefinitionCollection.php
@@ -0,0 +1,41 @@
+<?php
+
+/* 
+ * A collection that can only take Paths
+ */
+
+namespace Programster\Swagger;
+
+final class DefinitionCollection extends ArrayObject
+{
+    public function __construct(Definition ...$definitions)
+    {
+        parent::__construct($definitions);
+    }
+
+
+    public function append($value) 
+    {
+        if ($value instanceof Definition)
+        {
+            parent::append($value);
+        }
+        else
+        {
+            throw new Exception("Cannot append non Definition to a " . __CLASS__);
+        }
+    }
+
+
+    public function offsetSet($index, $newval) 
+    {
+        if ($newval instanceof Definition)
+        {
+            parent::offsetSet($index, $newval);
+        }
+        else
+        {
+            throw new Exception("Cannot add a non Definition value to a " . __CLASS__);
+        }
+    }
+}
---
+++ src/PathCollection.php
@@ -0,0 +1,41 @@
+<?php
+
+/* 
+ * A collection that can only take Paths
+ */
+
+namespace Programster\Swagger;
+
+final class PathCollection extends ArrayObject
+{
+    public function __construct(Path ...$paths)
+    {
+        parent::__construct($paths);
+    }
+
+
+    public function append($value) 
+    {
+        if ($value instanceof Path)
+        {
+            parent::append($value);
+        }
+        else
+        {
+            throw new Exception("Cannot append non Path to a " . __CLASS__);
+        }
+    }
+
+
+    public function offsetSet($index, $newval) 
+    {
+        if ($newval instanceof Path)
+        {
+            parent::offsetSet($index, $newval);
+        }
+        else
+        {
+            throw new Exception("Cannot add a non Path value to a " . __CLASS__);
+        }
+    }
+}
